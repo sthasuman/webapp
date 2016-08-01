a4c79a0eaa6a302f7639d2245d84615bf2b480be
Bibek Adhikari <bbekad94@gmail.com>
2016-08-01 14:30:02 +0545

    added vendor model

--- shoppingcart/models.py
+++ shoppingcart/models.py
@@ -13,3 +13,9 @@ class Vendor(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     dob = models.IntegerField(max_length=5)
+
+class Product(models.Model):
+    prod_name = models.CharField()
+    description = models.TextField()
+    price = models.IntegerField()
+
