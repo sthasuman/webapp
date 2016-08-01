2afb3b5a27575749b7faf1b235388bd5b21f5291
Suman Shrestha <me.sthasuman@gmail.com>
2016-08-01 14:21:05 +0545

    changed vendor model

--- shoppingcart/models.py
+++ shoppingcart/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Customer(models.Model):
+class Vendor(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    dateofbirth =models.DateTimeField()
-    address = models.CharField()
-    phone = models.IntegerField(max_length=10)
-    email = models.CharField()
-
+    dob = models.IntegerField(max_length=5)
